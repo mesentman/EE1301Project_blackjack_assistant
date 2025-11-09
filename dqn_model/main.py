930b696db69f114b33f4c8feb852dcd45bff1468
mesentman <mesentman2@gmail.com>
2025-11-08 23:40:16 -0600

    added quick train loop

--- dqn_model/main.py
+++ dqn_model/main.py
@@ -1,8 +1,10 @@
-from dql_agent import train_and_export, train_and_export_test
+from dql_agent import train_and_export, train_and_export_test, train_and_export_quick
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "test":
         train_and_export_test()
+    elif len(sys.argv) > 1 and sys.argv[1] == "quick":
+        train_and_export_quick()
     else:
         train_and_export(num_episodes=500_000)
